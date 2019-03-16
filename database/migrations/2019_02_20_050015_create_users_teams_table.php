dc0b3cc3052ca6667c7be8935ffc7247c094bc1f
= <chase.terry87@gmail.com>
2019-03-15 20:36:43 -0500

    Fixed migrations that associate users to teams and clients.

--- database/migrations/2019_02_20_050015_create_users_teams_table.php
+++ database/migrations/2019_02_20_050015_create_users_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeamsUsersTable extends Migration
+class CreateUsersTeamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateTeamsUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('teams_users', function (Blueprint $table) {
+        Schema::create('users_teams', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('team_id');
             $table->unsignedInteger('user_id');
+            $table->unsignedInteger('team_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateTeamsUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teams_users');
+        Schema::dropIfExists('users_teams');
     }
 }
