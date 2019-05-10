9d10e8d0ccef5560a60e54648c9a230866204e46
= <chase.terry87@gmail.com>
2019-05-09 22:39:59 -0500

    Adjusted the register and password reset request pages with materialize.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,62 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <h4 class="center-align">Proma</h4>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <hr/>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="row">
+        @include('layouts.errors')
     </div>
-</div>
+
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <div class="input-field">
+                <input type="text" id="name" name="name" maxlength="250" value="{{ old('name') }}" required>
+                <label for="name">Name</label>
+            </div>
+
+        <div class="input-field">
+            <input type="email" id="email" name="email" maxlength="250" value="{{ old('email') }}" required>
+            <label for="email">Email</label>
+        </div>
+
+        <div class="input-field">
+            <input type="password" id="password" name="password" required>
+            <label for="password">Password</label>
+        </div>
+
+        <div class="input-field">
+            <input id="password-confirm" type="password" name="password_confirmation" required>
+            <label for="password">Confirm Password</label>
+        </div>
+
+        <br/>
+
+        <div class="center-align">
+            <button type="submit" class="btn light-blue darken-2">
+                {{ __('Register') }}
+            </button>
+        </div>
+
+        <br>
+
+        <div class="center-align">
+            Already have an account?
+            <a href="{{ route('login') }}">
+                {{ __('Log in') }}
+            </a>
+        </div>
+
+        <br>
+
+        <hr/>
+
+        <div class="center-align">
+            Proma - Simple Project Management &copy; {{ date('Y') }}
+        </div>
+    </form>
+
 @endsection
