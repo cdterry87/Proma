6a91dd05eb9cfd52adc9d1aed26975298b0558c2
= <chase.terry87@gmail.com>
2019-05-24 22:43:09 -0500

    Added a new seeder for empty users.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,29 +19,25 @@ class UserSeeder extends Seeder
             'email' => 'admin@example.com',
             'password' => bcrypt('password'),
         ])->each(function ($admin) {
-            $admin->teams()->saveMany(factory(Team::class, 9)->create()->each(function ($team) {
+            $admin->teams()->saveMany(factory(Team::class, 30)->create()->each(function ($team) {
                 $team->members()->save(factory(TeamMember::class)->create([
                     'team_id' => $team->first()->id,
                     'user_id' => 1
                 ]));
             }));
-            $admin->clients()->saveMany(factory(Client::class, 9)->create()->each(function ($client) {
-                $client->contacts()->saveMany(factory(ClientContact::class, 9)->create([
+            $admin->clients()->saveMany(factory(Client::class, 30)->create()->each(function ($client) {
+                $client->contacts()->saveMany(factory(ClientContact::class, 30)->create([
                     'client_id' => $client->first()->id
                 ]));
             }));
-            $admin->projects()->saveMany(factory(Project::class, 9)->create()->each(function ($project) {
-                $project->tasks()->saveMany(factory(ProjectTask::class, 9)->create([
+            $admin->projects()->saveMany(factory(Project::class, 30)->create()->each(function ($project) {
+                $project->tasks()->saveMany(factory(ProjectTask::class, 30)->create([
                     'project_id' => $project->first()->id
                 ]));
             }));
         });
 
-        // Creating random users with associated teams, projects, and clients
-        $users = factory(User::class, 9)->create()->each(function ($user) {
-            $user->teams()->saveMany(factory(Team::class, 9)->make());
-            $user->clients()->saveMany(factory(Client::class, 9)->make());
-            $user->projects()->saveMany(factory(Project::class, 9)->make());
-        });
+        // Creating random users
+        factory(User::class, 9)->create();
     }
 }
