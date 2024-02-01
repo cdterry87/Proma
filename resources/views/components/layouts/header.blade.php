fbc35faafe5e401a20d334dfecfc7ef0e12d1198
Chase Terry <chase.terry87@gmail.com>
2024-01-31 21:34:06 -0600

    WIP Header links

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,13 +1,78 @@
 <div>
     <div class="navbar bg-base-300">
         <div class="w-full max-w-6xl mx-auto">
-            <div class="flex-1">
+            <div class="flex-1 flex items-center gap-1">
                 <a
                     href="{{ route('home') }}"
                     class="btn btn-ghost text-3xl logo"
                 >
                     {{ config('app.name') }}
                 </a>
+                <ul class="menu menu-horizontal px-1">
+                    <li>
+                        <a
+                            href="{{ route('projects') }}"
+                            class="flex items-center gap-1"
+                        >
+                            <x-icons.projects />
+                            <span class="hidden md:block">
+                                Projects
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="{{ route('issues') }}"
+                            class="flex items-center gap-1"
+                        >
+                            <x-icons.issues />
+                            <span class="hidden md:block">
+                                Issues
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="{{ route('clients') }}"
+                            class="flex items-center gap-1"
+                        >
+                            <x-icons.clients />
+                            <span class="hidden md:block">
+                                Clients
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <details>
+                            <summary class="flex items-center gap-1">
+                                <x-icons.admin />
+                                <span class="hidden md:block">
+                                    Administration
+                                </span>
+                            </summary>
+                            <ul class="p-2 bg-base-300">
+                                <li>
+                                    <a
+                                        href="{{ route('users') }}"
+                                        class="flex items-center gap-1"
+                                    >
+                                        <x-icons.users />
+                                        Users
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="{{ route('teams') }}"
+                                        class="flex items-center gap-1"
+                                    >
+                                        <x-icons.teams />
+                                        Teams
+                                    </a>
+                                </li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
             </div>
             <div class="flex-none gap-1">
                 <label
@@ -71,6 +136,4 @@
             </div>
         </div>
     </div>
-
-    <x-layouts.navigation />
 </div>
