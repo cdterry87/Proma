43ae30ef1a27760f23a18df6e8d5a79fdb053dd8
Chase Terry <chase.terry87@gmail.com>
2024-07-04 10:16:51 -0500

    Adds stats to dashboard

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,69 @@
 
 namespace App\Livewire;
 
+use App\Models\Issue;
+use App\Models\Project;
 use Livewire\Component;
 
 class Home extends Component
 {
+    public $days = 30;
+
     public function render()
     {
-        return view('livewire.home');
+        // Get number of projects created in the last 30 days
+        $projects = Project::where('user_id', auth()->id())->where('created_at', '>=', now()->subDays($this->days))->count();
+
+        // Get previous number of projects created in the 30 days before the last 30 days
+        $projects_previous = Project::where('user_id', auth()->id())->where('created_at', '>=', now()->subDays($this->days * 2))->where('created_at', '<', now()->subDays($this->days))->count();
+
+        // Calculate % change in projects created
+        $projects_change = $projects_previous ? (($projects - $projects_previous) / $projects_previous) * 100 : 0;
+
+        // Get number of projects completed in the last 30 days
+        $projects_completed = Project::where('user_id', auth()->id())->where('completed_date', '>=', now()->subDays($this->days))->count();
+
+        // Get previous number of projects completed in the 30 days before the last 30 days
+        $projects_completed_previous = Project::where('user_id', auth()->id())->where('completed_date', '>=', now()->subDays($this->days * 2))->where('completed_date', '<', now()->subDays($this->days))->count();
+
+        // Calculate % change in projects completed
+        $projects_completed_change = $projects_completed_previous ? (($projects_completed - $projects_completed_previous) / $projects_completed_previous) * 100 : 0;
+
+        // Get last 5 incomplete projects
+        $projects_incomplete = Project::where('user_id', auth()->id())->whereNull('completed_date')->orderBy('created_at', 'desc')->limit(5)->get();
+
+        // Get number of issues created in the last 30 days
+        $issues = Issue::where('user_id', auth()->id())->where('created_at', '>=', now()->subDays($this->days))->count();
+
+        // Get previous number of issues created in the 30 days before the last 30 days
+        $issues_previous = Issue::where('user_id', auth()->id())->where('created_at', '>=', now()->subDays($this->days * 2))->where('created_at', '<', now()->subDays($this->days))->count();
+
+        // Calculate % change in issues created
+        $issues_change = $issues_previous ? (($issues - $issues_previous) / $issues_previous) * 100 : 0;
+
+        // Get number of issues resolved in the last 30 days
+        $issues_resolved = Issue::where('user_id', auth()->id())->where('resolved_date', '>=', now()->subDays($this->days))->count();
+
+        // Get previous number of issues resolved in the 30 days before the last 30 days
+        $issues_resolved_previous = Issue::where('user_id', auth()->id())->where('resolved_date', '>=', now()->subDays($this->days * 2))->where('resolved_date', '<', now()->subDays($this->days))->count();
+
+        // Calculate % change in issues resolved
+        $issues_resolved_change = $issues_resolved_previous ? (($issues_resolved - $issues_resolved_previous) / $issues_resolved_previous) * 100 : 0;
+
+        // Get last 5 unresolved issues
+        $issues_unresolved = Issue::where('user_id', auth()->id())->whereNull('resolved_date')->orderBy('created_at', 'desc')->limit(5)->get();
+
+        return view('livewire.home', [
+            'projects' => $projects,
+            'projects_completed' => $projects_completed,
+            'projects_change' => $projects_change,
+            'projects_completed_change' => $projects_completed_change,
+            'issues' => $issues,
+            'issues_resolved' => $issues_resolved,
+            'issues_change' => $issues_change,
+            'issues_resolved_change' => $issues_resolved_change,
+            'projects_incomplete' => $projects_incomplete,
+            'issues_unresolved' => $issues_unresolved
+        ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -6,6 +6,7 @@
                 name="days"
                 label="Select Timeframe"
                 hide-label
+                wire:model="days"
             >
                 <option value="30">Last 30 days</option>
                 <option value="90">Last 90 days</option>
@@ -14,33 +15,101 @@
             </x-inputs.select>
         </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div class="stats shadow bg-base-200">
-
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div class="stats stats-vertical sm:stats-horizontal xl:stats-horizontal shadow bg-base-200 w-full">
             <div class="stat">
-                <div class="stat-title">Projects</div>
-                <div class="stat-value">0</div>
-                <div class="stat-desc">21% more than last month</div>
+                <div class="stat-figure text-primary">
+                    <x-icons.projects />
+                </div>
+                <div class="stat-title">Projects Started</div>
+                <div class="stat-value">{{ $projects }}</div>
+                <div class="stat-desc">{{ $projects_change }}% Increased/Decreased</div>
             </div>
-
-        </div>
-        <div class="stats shadow bg-base-200">
-
             <div class="stat">
-                <div class="stat-title">Issues</div>
-                <div class="stat-value">0</div>
-                <div class="stat-desc">21% more than last month</div>
+                <div class="stat-figure text-success">
+                    <x-icons.success />
+                </div>
+                <div class="stat-title">Projects Completed</div>
+                <div class="stat-value">{{ $projects_completed }}</div>
+                <div class="stat-desc">{{ $projects_completed_change }}% Increased/Decreased</div>
             </div>
-
         </div>
-        <div class="stats shadow bg-base-200">
-
+        <div class="stats stats-vertical sm:stats-horizontal xl:stats-horizontal shadow bg-base-200 w-full">
             <div class="stat">
-                <div class="stat-title">Time Spent</div>
-                <div class="stat-value">89,400</div>
-                <div class="stat-desc">21% increase/decrease</div>
+                <div class="stat-figure text-primary">
+                    <x-icons.issues />
+                </div>
+                <div class="stat-title">Issues Opened</div>
+                <div class="stat-value">{{ $issues }}</div>
+                <div class="stat-desc">{{ $issues_change }}% Increased/Decreased</div>
             </div>
-
+            <div class="stat">
+                <div class="stat-figure text-success">
+                    <x-icons.success />
+                </div>
+                <div class="stat-title">Issues Resolved</div>
+                <div class="stat-value">{{ $issues_resolved }}</div>
+                <div class="stat-desc">{{ $issues_resolved_change }}% Increased/Decreased</div>
+            </div>
         </div>
+
+        <x-layouts.card title="Latest Incomplete Projects">
+            @if ($projects_incomplete && $projects_incomplete->isNotEmpty())
+                <div class="overflow-x-auto bg-base-100 rounded">
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Project</th>
+                                <th>Due Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects_incomplete as $project)
+                                <tr>
+                                    <td class="w-full">
+                                        <a
+                                            href="{{ route('projects.view', $project->id) }}"
+                                            class="text-primary font-bold hover:brightness-125"
+                                        >{{ $project->name }}</a>
+                                    </td>
+                                    <td>{{ $project->due_date ?? 'N/A' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <p>No incomplete projects found.</p>
+            @endif
+        </x-layouts.card>
+        <x-layouts.card title="Latest Issues">
+            @if ($issues_unresolved && $issues_unresolved->isNotEmpty())
+                <div class="overflow-x-auto bg-base-100 rounded">
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Issue</th>
+                                <th>Open Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($issues_unresolved as $issue)
+                                <tr>
+                                    <td class="w-full">
+                                        <a
+                                            href="{{ route('issues.view', $issue->id) }}"
+                                            class="text-primary font-bold hover:brightness-125"
+                                        >{{ $issue->name }}</a>
+                                    </td>
+                                    <td>{{ $issue->created_at->format('m/d/Y') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <p>No unresolved issues found.</p>
+            @endif
+        </x-layouts.card>
     </div>
 </div>
