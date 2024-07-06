90070af1ca100a1bd2cd149e955a54492682cac4
Chase Terry <chase.terry87@gmail.com>
2024-07-05 23:41:22 -0500

    Close #42. Adds deploy scripts.

---
+++ server_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --render=errors/503) || true
+
+# Update codebase
+git fetch origin deploy
+git reset --hard origin/deploy
+
+# Install dependencies based on lock file
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Migrate database
+php artisan migrate --force
+
+# Reload PHP to update opcache
+echo "" | sudo -S service php8.2-fpm reload
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
