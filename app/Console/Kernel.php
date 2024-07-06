8ddb587217990d79c6f2a1b584fa4d9efa3ec66c
Chase Terry <chase.terry87@gmail.com>
2024-07-06 15:24:24 -0500

    #44. Adds routine cleanup job to console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->job(new \App\Jobs\IssuesNotificationsJob())->dailyAt('08:00');
+        // $schedule->job(new \App\Jobs\ProjectsNotificationsJob())->dailyAt('08:00');
+        // $schedule->job(new \App\Jobs\ProjectsTasksNotificationsJob())->dailyAt('08:00');
+
+        $schedule->job(new \App\Jobs\RoutineCleanupJob())->dailyAt('00:00');
     }
 
     /**
@@ -20,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
